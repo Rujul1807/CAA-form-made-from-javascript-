d46c4dd64c097e54ec9c43211befe23fb823114d
Rujul1807 <60806188+Rujul1807@users.noreply.github.com>
2020-06-22 02:13:46 +0530

    Update frontForm.js

--- frontForm.js
+++ frontForm.js
@@ -1,18 +1,19 @@
 class frontForm{
 constructor(){
-    this.input=createInput("your emailId");
-    this.start=createButton('start');
+    this.input=createInput("Your email Id");
+    this.start=createButton('Start');
     this.ques=createElement('h3')
     this.ques4=createElement('h3')
     this.ques2=createElement('h3')
     this.ques3=createElement('h3')
-    this.answer1=createInput("your answer");
-    this.answer2=createInput("your answer");
-    this.answer3=createInput("your answer");
-    this.answer4=createInput("your answer");
-    this.submit=createButton('sumbit');
+    this.answer1=createInput("Your answer");
+    this.answer2=createInput("Your answer");
+    this.answer3=createInput("Your answer");
+    this.answer4=createInput("Your answer");
+    this.submit=createButton('Submit');
      this.topic=createElement('h1');
      this.salut=createElement('h1');
+    
 
 }
 hide(){
@@ -34,26 +35,27 @@ display(){
 //this.ques3.html("");
  this.input.position(displayWidth/2-300,displayHeight/2-100);
  this.start.position(displayWidth/2-300,displayHeight/2-50);
- this.topic.html("YOUR CAA FORM")
+ this.topic.html("CAA SURVEY QUESTIONNAIRE");
  this.topic.position(displayWidth/2-300,displayHeight/2-300);
   this.start.mousePressed(()=>{
   this.input.hide();
   this.start.hide();
   voter.email=this.input.value(); 
-  this.ques.html("Do you support CAA for our country");
+  
+  this.ques.html("Do you support Citizenship Amendment Act 2019(CAA) in our country");
   this.ques.position(displayWidth/2-600,displayHeight/2-200);
   this.answer1.position(displayWidth/2-500,displayHeight/2-150); 
 
-  this.ques2.html("Do yo think that there should be any change in the amendment");
+  this.ques2.html("Do yo think that there should be any change in the amendment,if yes,then what?");
   this.ques2.position(displayWidth/2-600,displayHeight/2-100);
   this.answer2.position(displayWidth/2-500,displayHeight/2-50);
 
-  this.ques3.html("suppose you have a  very good muslim friend in town and he wants citizenship in our country do you think it is right for him  not to have a citizenship though he intends to do no harm ");
+  this.ques3.html("Do you think we need to have a National Register in India?");
   this.ques3.position(displayWidth/2-600,displayHeight/2); 
   this.answer3.position(displayWidth/2-500,displayHeight/2+100)
  
   
-  this.ques4.html("Do you think that CAA is important for our country");
+  this.ques4.html("What benefit CAA will provide to national security?");
   this.ques4.position(displayWidth/2-600,displayHeight/2+150);
   this.answer4.position(displayWidth/2-500,displayHeight/2+200);
 
@@ -80,8 +82,8 @@ display(){
     this.submit.hide();
      this.topic.hide();
   
-     this.salut.html("THANKYOU FOR TRYING OUR FORM");
-     this.salut.position(displayWidth/2-200,displayHeight/2)
+     this.salut.html("THANK YOU FOR PARTICIPATING IN THIS SURVEY");
+     this.salut.position(displayWidth/2-300,displayHeight/2)
 
  })
  
